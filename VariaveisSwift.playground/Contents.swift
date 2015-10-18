9bbfbafb1e0fb5097964f5ff92a89f9e18d702d7
mvalbuquerque <albuquerquenet@icloud.com>
2015-10-18 19:12:26 -0200

    SampleCode

---
+++ VariaveisSwift.playground/Contents.swift
@@ -0,0 +1,39 @@
+//: Playground - noun: a place where people can play
+//Twitter: @mvalbuquerque
+
+
+import UIKit
+
+
+// Variável do tipo string
+var str = "mvalbuquerque"
+
+// Variável do tipo inteira
+var Inteira : Int = 10
+
+// Variável do tipo float
+var Float : Float = 33.6
+
+// Variável do tipo double
+var Double : Double = 122.334
+
+// Variável do tipo Booleana
+var Bool : Bool = true  // or false
+
+
+//Imprime o valor da variável str
+print("o valor da variável str é:",str)
+
+//Imprime o valor da variável Inteira
+print("o valor da variável Inteira é:", Inteira)
+
+//Imprime o valor da variável Float
+print("o valor da variável Float é:",Float)
+
+//Imprime o valor da variável Double
+print("o valor da variável Double é:", Double)
+
+//Imprime o valor da vairável Bool
+print("o valor da variável Bool é:", Bool)
+
+
